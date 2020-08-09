61f060449eaff51418b8ec2c7afcc4dee884c19a
bertac_severcan <bertac.severcan@icloud.com>
2020-08-09 23:20:38 +0300

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,21 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function  superbowlWin(arr) {
+  const res = arr.find(r => r.result === "W")
+  if(res !== undefined){
+    return res.year
+  }
+  return res
+}
+
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "L"},
+  {year: "2016", result: "N/A"}
+  //...
+]
+let a = superBowlWin(record)
+
+console.log(a)
